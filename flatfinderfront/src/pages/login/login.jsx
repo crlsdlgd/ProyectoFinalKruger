599c8fd15efce43227ec8bdebddb5d673488ff5a
Carlos Delgado <crlsdlgd1992@gmail.com>
2025-04-06 17:46:34 -0500

    add heroui configuration

--- flatfinderfront/src/pages/login/login.jsx
+++ flatfinderfront/src/pages/login/login.jsx
@@ -1,9 +1,26 @@
+import { Input } from "@heroui/input";
+import "./login.css";
+
 const Login = () => {
   return (
     <div>
       <h1>Login</h1>
+      <Input
+        label="Email"
+        type="email"
+        className="email-input"
+        variant="faded"
+        isRequired
+        color="primary"
+      />
+      <Input
+        label="Password"
+        type="password"
+        className="email-input"
+        color="success"
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Login;
