3696ec7014252b0671f30bc75f28a28ee2c01f5c
crlsdlgd <crlsdlgd1992@gmail.com>
2025-04-08 18:11:24 -0500

    stilos basicos del login

--- flatfinderfront/src/pages/login/login.jsx
+++ flatfinderfront/src/pages/login/login.jsx
@@ -1,25 +1,55 @@
 import { Input } from "@heroui/input";
+import { Button } from "@heroui/button";
+import { useState } from "react";
 import "./login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <Input
-        label="Email"
-        type="email"
-        className="email-input"
-        variant="faded"
-        isRequired
-        color="primary"
-      />
-      <Input
-        label="Password"
-        type="password"
-        className="email-input"
-        color="success"
-      />
-    </div>
+    <main className="login-container">
+      <section className="login-form-container">
+        <form onSubmit={(e) => handleLogin(e)} className="login-form">
+          <h3 className="login-title">Sign In</h3>
+          <div>
+            <Input
+              label="Email"
+              type="email"
+              className="login-input"
+              isRequired
+              variant="underlined"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoComplete="off"
+            />
+            <Input
+              label="Password"
+              type="password"
+              className="login-input"
+              isRequired
+              variant="underlined"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="off"
+            />
+            <Button type="submit" className="login-button">
+              LOGIN
+            </Button>
+          </div>
+          <p className="login-link">
+            Don't have an account? <a href="/register">Sign Up</a>
+          </p>
+        </form>
+      </section>
+    </main>
   );
 };
 
