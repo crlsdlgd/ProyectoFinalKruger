3fddc5ae60f86d13fcbf379c3e39901730a2b572
Galoorellana <gorellana@krugerschool.edu.ec>
2025-04-29 22:08:51 -0500

    implement user management functions: getUserById, getAllUsers, updateUser, deleteUser; update sanitizeBody middleware formatting

--- flatfinderback/controllers/user.controller.js
+++ flatfinderback/controllers/user.controller.js
@@ -20,6 +20,62 @@ const loginUser = async (req,res) =>{
     res.status(401).json({message:"Invalid email or password" });
 
   }
-}
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+const updateUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(deletedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
 
 export { saveUser , loginUser };
