0078486d8a28ccb49969993b3021a5358aa30e90
Carlos Delgado <crlsdlgd1992@gmail.com>
2025-05-13 18:56:11 -0500

    add inspiration desing images

--- flatfinderfront/tailwind.config.js
+++ flatfinderfront/tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 import { heroui } from "@heroui/react";
 
 /** @type {import('tailwindcss').Config} */
@@ -7,9 +8,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
   darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#00BCD4",     // Claro
+          dark: "#26A69A",        // Oscuro
+        },
+        background: {
+          DEFAULT: "#ffffff",
+          dark: "#121212",
+        },
+        text: {
+          DEFAULT: "#000000",
+          dark: "#ffffff",
+        },
+      },
+    },
+  },
   plugins: [heroui()],
 };
