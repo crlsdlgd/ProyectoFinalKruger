0e2d1316daab33e4f6f8ef0757d20226645f3bc8
Carlos Delgado <crlsdlgd1992@gmail.com>
2025-05-17 15:26:06 -0500

    validate flat data

--- flatfinderback/models/flat.model.js
+++ flatfinderback/models/flat.model.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const flatSchema = new mongoose.Schema({
-  city: { type: String, required: [true, "city is required"] },
-  streetName: { type: String, required: [true, "Street name is required"] },
+  city: { type: String, minlength: [2, "City must be at least 2 characters long"], required: [true, "city is required"] },
+  streetName: { type: String, minlength: [2, "Street Name must be at least 2 characters long"], required: [true, "Street name is required"] },
   streetNumber: { type: Number, min: [0, "Street number must be greater than 0"], required: [true, "Street number is required"] },
   hasAC: { type: Boolean, required: [true, "has AC is required"] },
   yearBuilt: { type: Number, min: [1950, "Year built must be greater than 1950"], required: [true, "Year built is required"] },
